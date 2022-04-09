82683f14077b503ac8d918cea2006da16a289ea0
Roby Karti S S.KOM <55522784+robykartis@users.noreply.github.com>
2022-04-10 02:13:50 +0700

    commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Redirect;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get all posts from database
+        $posts = Post::latest()->get();
+
+        //render with data "posts"
+        return Inertia::render('Post/Index', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Post/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //set validation
+        $request->validate([
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        //create post
+        $post = Post::create([
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        if ($post) {
+            return Redirect()->route('posts.index')->with('message', 'Data Berhasil Disimpan!');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return Inertia::render('Post/Edit', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //set validation
+        $request->validate([
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        //update post
+        $post->update([
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        if ($post) {
+            return Redirect::route('posts.index')->with('message', 'Data Berhasil Diupdate!');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //find post by ID
+        $post = Post::findOrfail($id);
+
+        //delete post
+        $post->delete();
+
+        if ($post) {
+            return Redirect()->route('posts.index')->with('message', 'Data Berhasil Dihapus!');
+        }
+    }
+}
